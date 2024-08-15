2901ef4e806aca16d019d4c072107e21218fe341
Angel Dhakal <51156237+andy4747@users.noreply.github.com>
2024-08-15 01:04:15 +0000

    array: selection sort done

--- arrays/selection_sort.go
+++ arrays/selection_sort.go
@@ -1,3 +1,16 @@
 package arrays
 
-func SelectionSort(arr []int) {}
+func SelectionSort(arr []int) {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		minIdx := i
+		for j := i + 1; j < n; j++ {
+			if arr[minIdx] > arr[j] {
+				minIdx = j
+			}
+		}
+		if minIdx != i {
+			arr[minIdx], arr[i] = arr[i], arr[minIdx]
+		}
+	}
+}
