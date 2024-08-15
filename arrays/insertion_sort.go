95e5708270c29f2357dfde12c429e68f54ce83e4
andy4747 <angeldhakal97@gmail.com>
2024-08-15 14:28:59 -0400

    docs: insertion sort docs added

--- arrays/insertion_sort.go
+++ arrays/insertion_sort.go
@@ -1,3 +1,16 @@
+/*
+Author: Angel Dhakal
+File: insertion_sort.go
+Date: 2024-08-15
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+Problem: Insertion sort an array
+Solution:{
+start at index 1, put element of i in temp and create a space at ith index, and compare the temp to the previous element,
+and if pe>temp, shift the pe to the right once(in the gap), update the gap to be -1 than last time, and
+again check if space>0 and pe>temp and continue. and if space is 0 or temp>pe then put temp in the final space
+}
+*/
 package arrays
 
 func InsertionSort(arr []int) {
