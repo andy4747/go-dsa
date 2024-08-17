9b52ea44f59086347b1b4a8f00df5f9b08ce42d2
andy4747 <angeldhakal97@gmail.com>
2024-08-17 14:19:38 -0400

    docs: right_angled_number_pyramid docs

--- patterns/right_angled_number_pyramid.go
+++ patterns/right_angled_number_pyramid.go
@@ -1,3 +1,11 @@
+/*
+Author: Angel Dhakal
+File: right_angled_number_pyramid.go
+Date: 2024-08-17
+Problem: https://takeuforward.org/pattern/pattern-3-right-angled-number-pyramid/
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+*/
 package patterns
 
 import "fmt"
