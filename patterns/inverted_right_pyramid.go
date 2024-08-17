dac98d3d495eea97057cda82a402d9b065c33976
andy4747 <angeldhakal97@gmail.com>
2024-08-17 14:47:00 -0400

    docs: inverted right pyramid docs

--- patterns/inverted_right_pyramid.go
+++ patterns/inverted_right_pyramid.go
@@ -1,3 +1,11 @@
+/*
+Author: Angel Dhakal
+File: inverted_right_pyramid.go
+Date: 2024-08-17
+Problem: https://takeuforward.org/pattern/pattern-5-inverted-right-pyramid/
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+*/
 package patterns
 
 import "fmt"
