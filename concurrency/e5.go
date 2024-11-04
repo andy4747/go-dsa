ce9ee0e6c777b5c9ee40269c91f162afbd381a67
Angel Dhakal <51156237+andy4747@users.noreply.github.com>
2024-11-04 00:34:30 +0000

    concurrency: e5: number squarring with channels

--- concurrency/e5.go
+++ concurrency/e5.go
@@ -1,4 +1,8 @@
-package concurrency
+package main
+
+import (
+  "fmt"
+)
 
 /*
 ### E5. Number Squaring with Channels
@@ -10,3 +14,33 @@ package concurrency
   - Process 5 numbers
 - **Learning Focus:** Channel direction, data processing
 */
+
+func square(inp_ch chan int, out_ch chan int) {
+  for num := range inp_ch {
+    out_ch <- num * num
+  }
+  close(out_ch)
+}
+
+func main() {
+  inp_ch := make(chan int)
+  out_ch := make(chan int)
+
+  nums := []int{2,3,4,5,6}
+
+
+  go square(inp_ch, out_ch)
+
+  go func() {
+    for _, v := range nums {
+      inp_ch <- v
+    }
+    close(inp_ch)
+  } ()
+
+  for v := range out_ch {
+    fmt.Println(v)
+  }
+
+  fmt.Println("Program Completed")
+}
